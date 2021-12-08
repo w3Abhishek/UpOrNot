c7051afb176622f4c5baa06a876577feecdefd6d
Abhishek Verma <abhishekverma3124@gmail.com>
2021-12-08 23:55:58 +0530

    changed the requirements

--- check.py
+++ check.py
@@ -2,6 +2,10 @@ import requests
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 def check_status(url):
     url = 'http://' + url
-    response = requests.get(url, headers=headers)
-    status_response = {'status' : response.status_code}
-    return status_response
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=True)
+        status_response = {'status' : response.status_code}
+        return status_response
+    except:
+        status_response = {'status' : 404}
+        return status_response
